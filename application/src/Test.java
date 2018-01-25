5303eb56d4523f6a10de934b88ceaab7c650ebec
piropaolo <piropaolo27@gmail.com>
2018-01-25 23:49:06 +0100

    Implement database populating and deleting testing methods

--- application/src/Test.java
+++ application/src/Test.java
@@ -4,22 +4,45 @@ import java.sql.SQLException;
 import java.sql.Types;
 
 public class Test {
-    public static int test(Connection connection) {
+    public static float populate(Connection connection) {
         CallableStatement callableStatement;
         try {
-            callableStatement = connection.prepareCall("BEGIN TESUTO(?); END;");
+            callableStatement = connection.prepareCall("BEGIN POPULATE(?); END;");
             callableStatement.registerOutParameter(1, Types.NUMERIC);
         } catch (SQLException e) {
             callableStatement = null;
             e.printStackTrace();
         }
-//        try {
-//            assert callableStatement != null;
-//            callableStatement.execute();
-//            return callableStatement.getInt(1);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            assert callableStatement != null;
+            long startTime = System.currentTimeMillis();
+            callableStatement.execute();
+            long endTime   = System.currentTimeMillis();
+            return endTime - startTime;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public static float delete(Connection connection) {
+        CallableStatement callableStatement;
+        try {
+            callableStatement = connection.prepareCall("BEGIN DELETE_FROM_TABLES(?); END;");
+            callableStatement.registerOutParameter(1, Types.NUMERIC);
+        } catch (SQLException e) {
+            callableStatement = null;
+            e.printStackTrace();
+        }
+        try {
+            assert callableStatement != null;
+            long startTime = System.currentTimeMillis();
+            callableStatement.execute();
+            long endTime   = System.currentTimeMillis();
+            return endTime - startTime;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return -1;
     }
 }
